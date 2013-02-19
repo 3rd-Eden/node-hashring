b48a65ed70fd5e0f406e6e35f06af80cf5d50108
3rd-Eden <info@3rd-Eden.com>
2013-02-19 23:54:07 +0100

    [test] Fixing regressions and updating test suite to the new internals

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ var hashValue = require('./build/Release/hashvalue')
  * @api private
  */
 parse.extension('vnodes', function vnode(data, value) {
-  if (value && 'vnodes' in value) {
+  if (typeof value === 'object' && !Array.isArray(value) && 'vnodes' in value) {
     data.vnodes = +value.vnodes || 0;
   } else {
     data.vnodes = 0;
@@ -74,14 +74,17 @@ HashRing.prototype.continuum = function generate() {
     , index = 0
     , total;
 
+  // No servers, bailout
+  if (!servers.length) return this;
+
   // Generate the total weight of all the servers
   total = servers.reduce(function reduce(total, server) {
-    return total += server.weight;
+    return total + server.weight;
   }, 0);
 
   servers.forEach(function each(server) {
     var percentage = server.weight / total
-      , vnodes = self.vnodes[server] || self.vnodes
+      , vnodes = self.vnodes[server.string] || self.vnode
       , length = Math.floor(percentage * vnodes * servers.length)
       , key
       , x;
@@ -103,6 +106,7 @@ HashRing.prototype.continuum = function generate() {
   });
 
   this.size = this.ring.length;
+  return this;
 };
 
 /**
@@ -114,8 +118,26 @@ HashRing.prototype.continuum = function generate() {
  * @api public
  */
 HashRing.prototype.get = function get(key) {
-  var hashValue = this.hashValue(key)
-    , ring = this.ring
+  var cache = this.cache.get(key);
+  if (cache) return cache;
+
+  var node = this.ring[this.find(this.hashValue(key))];
+  if (!node) return undefined;
+
+  this.cache.set(key, node.server);
+  return node.server;
+};
+
+/**
+ * Find the correct node for the key which is closest to the point after what
+ * the given key hashes to.
+ *
+ * @param {Number} hashValue find the nearest server close to this hash
+ * @returns {Number} position of the server in the hash ring
+ * @api public
+ */
+HashRing.prototype.find = function find(hashValue) {
+  var ring = this.ring
     , high = this.size
     , low = 0
     , middle
@@ -125,14 +147,14 @@ HashRing.prototype.get = function get(key) {
   // Preform a search on the array to find the server with the next biggest
   // point after what the given key hashes to
   while (true) {
-    mid = (low + high) / 2;
+    mid = (low + high) >> 1;
 
-    if (mid === this.size) return ring[0].server;
+    if (mid === this.size) return 0;
 
     middle = ring[mid].value;
     prev = mid === 0 ? 0 : ring[mid - 1].value;
 
-    if (hashValue <= middle && hashValue > prev) return ring[mid].server;
+    if (hashValue <= middle && hashValue > prev) return mid;
 
     if (middle < hashValue) {
       low = mid + 1;
@@ -140,7 +162,7 @@ HashRing.prototype.get = function get(key) {
       high = mid - 1;
     }
 
-    if (low > high) return ring[0].server;
+    if (low > high) return 0;
   }
 };
 
@@ -169,25 +191,6 @@ HashRing.prototype.digest = function digest(key) {
 };
 
 /**
- * None ketama:
- * The following changes are not ported from the ketama algorithm and are hash
- * ring specific. Add, remove or replace servers with as less disruption as
- * possible.
- */
-
-HashRing.prototype.replace = function replace(from, to) {
-
-};
-
-HashRing.prototype.add = function add(servers) {
-
-};
-
-HashRing.prototype.remove = function remove(server) {
-
-};
-
-/**
  * Get the hashed value for the given key
  *
  * @param {String} key
@@ -198,6 +201,129 @@ HashRing.prototype.hashValue = function hasher(key) {
   var x = this.digest(key);
 
   return hashValue.hash(x[3], x[2], x[1], x[0]);
+};
+
+/**
+ * None ketama:
+ * The following changes are not ported from the ketama algorithm and are hash
+ * ring specific. Add, remove or replace servers with as less disruption as
+ * possible.
+ */
+
+/**
+ * Hotswap identical servers with each other. This doesn't require the cache to
+ * be completely nuked and the hash ring distribution to be re-calculated.
+ *
+ * Please note that removing the server and a new adding server could
+ * potentially create a different distribution.
+ *
+ * @param {String} from The server that needs to be replaced
+ * @param {String} to The server that replaces the server
+ * @api public
+ */
+HashRing.prototype.swap = function swap(from, to) {
+  var connection = parse(to).servers.pop()
+    , self = this;
+
+  this.ring.forEach(function forEach(node) {
+    if (node.server === from) node.server = to;
+  });
+
+  this.cache.forEach(function forEach(value, key) {
+    if (value === from) self.cache.set(key, to);
+  }, this);
+
+  // Update the virtual nodes
+  this.vnodes[to] = this.vnodes[from];
+  delete this.vnodes[from];
+
+  // Update the servers
+  this.servers = this.servers.map(function mapswap(server) {
+    if (server.string === from) {
+      server.string = to;
+      server.host = connection.host;
+      server.port = connection.port;
+    }
+
+    return server;
+  });
+
+  return this;
+};
+
+/**
+ * Add a new server to ring without having to re-initialize the hashring. It
+ * accepts the same arguments as you can use in the constructor.
+ *
+ * @param {Mixed} servers Servers that need to be added to the ring
+ * @api public
+ */
+HashRing.prototype.add = function add(servers) {
+  var connections = Object.create(null);
+
+  // Add the current servers to the set.
+  this.servers.forEach(function forEach(server) {
+    connections[server.string] = server;
+  });
+
+  parse(servers).servers.forEach(function forEach(server) {
+    // Don't add duplicate servers
+    if (server.string in connections) return;
+    connections[server.string] = server;
+  });
+
+  // Now that we generated a complete set of servers, we can update the re-parse
+  // the set and correctly added all the servers again
+  connections = parse(connections);
+  this.vnodes = connections.vnodes;
+  this.servers = connections.servers;
+
+  // Rebuild the hash ring
+  this.reset();
+  return this.continuum();
+};
+
+/**
+ * Remove a server from the hashring.
+ *
+ * @param {Mixed} server
+ * @api public
+ */
+HashRing.prototype.remove = function remove(server) {
+  var connection = parse(server).servers.pop();
+
+  delete this.vnodes[connection.string];
+  this.servers = this.servers.map(function map(server) {
+    if (server.string === connection.string) return undefined;
+
+    return server;
+  }).filter(Boolean);
+
+  // Rebuild the hash ring
+  this.reset();
+  return this.continuum();
+};
+
+/**
+ * Reset the HashRing to clean up all references
+ *
+ * @api public
+ */
+HashRing.prototype.reset = function reset() {
+  this.ring.length = 0;
+  this.size = 0;
+  this.cache.reset();
+
+  return this;
+};
+
+HashRing.prototype.end = function end() {
+  this.reset();
+
+  this.vnodes = {};
+  this.servers.length = 0;
+
+  return this;
 };
 
 /**
@@ -214,14 +340,47 @@ function Node(hashvalue, server) {
 }
 
 /**
- * Set up the legacy API aliases.
+ * Set up the legacy API aliases. These will be depricated in the next release.
  *
  * @api public
  */
-HashRing.prototype.replaceServer = HashRing.prototype.replace;
-HashRing.prototype.removeServer = HashRing.prototype.remove;
-HashRing.prototype.addServer = HashRing.prototype.add;
-HashRing.prototype.getNode = HashRing.prototype.get;
+[
+  { from: 'replaceServer' },
+  { from: 'replace' },
+  { from: 'removeServer', to: 'remove' },
+  { from: 'addServer', to: 'add' },
+  { from: 'getNode', to: 'get' },
+  { from: 'getNodePosition', to: 'find' },
+  { from: 'position', to: 'find' }
+].forEach(function depricate(api) {
+  var notified = false;
+
+  HashRing.prototype[api.from] = function depricating() {
+    if (!notified) {
+      console.warn();
+      console.warn('[depricated] HashRing#'+ api.from +' is removed.');
+
+      // Not every API has a replacement API that should be used
+      if (api.to) {
+        console.warn('[depricated] use HashRing#'+ api.to +' as replacement.');
+      } else {
+        console.warn('[depricated] the API has no replacement');
+      }
+
+      console.warn();
+      notified = true;
+    }
+
+    if (api.to) return HashRing.prototype[api.to].apply(this, arguments);
+  };
+});
+
+/**
+ * Expose the current version number.
+ *
+ * @type {String}
+ */
+HashRing.version = require('./package.json').version;
 
 /**
  * Expose the module.
--- tests/hashring.test.js
+++ tests/hashring.test.js
@@ -10,9 +10,8 @@ module.exports = {
 , 'Constructing with a string': function () {
     var ring = new Hashring('192.168.0.102:11212');
 
-    ring.nodes.should.have.length(1);
-    ring.sortedKeys.length.should.be.above(1);
-    Object.keys(ring.weights).should.have.length(0);
+    ring.servers.should.have.length(1);
+    ring.ring.length.should.be.above(1);
   }
 
 , 'Constructing with a array': function () {
@@ -22,67 +21,66 @@ module.exports = {
       , '192.168.0.104:11212'
     ]);
 
-    ring.nodes.should.have.length(3);
-    ring.sortedKeys.length.should.be.above(1);
-    Object.keys(ring.weights).should.have.length(0);
+    ring.servers.should.have.length(3);
+    ring.ring.length.should.be.above(1);
   }
 
 , 'Constructing with a object': function () {
     var ring = new Hashring({
-        '192.168.0.102:11212': 1
+        '192.168.0.102:11212': 2
       , '192.168.0.103:11212': 2
-      , '192.168.0.104:11212': 1
+      , '192.168.0.104:11212': 2
     });
 
-    ring.nodes.should.have.length(3);
-    ring.sortedKeys.length.should.be.above(1);
-    Object.keys(ring.weights).should.have.length(3);
+    ring.servers.should.have.length(3);
+    ring.ring.length.should.be.above(1);
+
+    ring.servers.forEach(function (server) {
+      server.weight.should.be.above(1);
+    });
   }
 
 , 'Constructing with a object with pernode vnodes': function(){
     var ring = new Hashring({
-        '192.168.0.102:11212': {"vnodes":40}
-      , '192.168.0.103:11212': {"vnodes":50}
-      , '192.168.0.104:11212': {"vnodes":5}
+        '192.168.0.102:11212': { 'vnodes': 40 }
+      , '192.168.0.103:11212': { 'vnodes': 50 }
+      , '192.168.0.104:11212': { 'vnodes': 5 }
     });
 
-    ring.nodes.should.have.length(3);
-    ring.sortedKeys.length.should.be.equal((40+50+5)*3);
+    ring.servers.should.have.length(3);
+    ring.ring.length.should.be.equal((40 + 50 + 5) * 3);
 
     ring = new Hashring({
-        '192.168.0.102:11212': {"vnodes":4}
-      , '192.168.0.103:11212': {"vnodes":3}
-      , '192.168.0.104:11212': {"vnodes":5}
+        '192.168.0.102:11212': { 'vnodes': 4 }
+      , '192.168.0.103:11212': { 'vnodes': 3 }
+      , '192.168.0.104:11212': { 'vnodes': 5 }
     });
 
-    ring.nodes.should.have.length(3);
-    ring.sortedKeys.length.should.be.equal((4+3+5)*3);
+    ring.servers.should.have.length(3);
+    ring.ring.length.should.be.equal((4 + 3 + 5) * 3);
   }
 
 , 'Constructing with a different algorithm': function () {
     var ring = new Hashring('192.168.0.102:11212', 'md5');
 
-    ring.nodes.should.have.length(1);
+    ring.servers.should.have.length(1);
     ring.algorithm.should.equal('md5');
-    ring.sortedKeys.length.should.be.above(1);
-    Object.keys(ring.weights).should.have.length(0);
+    ring.ring.length.should.be.above(1);
   }
 
 , 'Constructing with no arguments': function () {
     var ring = new Hashring();
 
-    ring.nodes.should.have.length(0);
-    ring.sortedKeys.should.have.length(0);
-    Object.keys(ring.weights).should.have.length(0);
+    ring.servers.should.have.length(0);
+    ring.ring.should.have.length(0);
   }
 
 , 'Adding server after zero-argument constructor': function () {
     var ring = new Hashring();
     ring.add('192.168.0.102:11212');
 
-    ring.nodes.should.have.length(1);
-    ring.sortedKeys.length.should.be.above(1);
-    Object.keys(ring.weights).should.have.length(0);
+    ring.servers.should.have.length(1);
+    ring.ring.length.should.be.above(1);
   }
 
 , 'Looking up keys': function () {
@@ -92,27 +90,28 @@ module.exports = {
       , '192.168.0.104:11212'
     ]);
 
-    ring.nodes.indexOf(ring.get('foo')).should.be.above(-1);
+    ring.find(ring.hashValue('foo')).should.be.above(-1);
 
     // NOTE we are going to do some flaky testing ;P
-    ring.get('foo').should.equal('192.168.0.104:11212');
-    ring.get('pewpew').should.equal('192.168.0.103:11212');
+    ring.get('foo').should.equal('192.168.0.102:11212');
+    ring.get('pewpew').should.equal('192.168.0.104:11212');
 
     // we are not gonna verify the results we are just gonna test if we don't
     // fuck something up in the code, so it throws errors or whatever
 
     // unicode keys, just because people roll like that
-    ring.nodes.indexOf(ring.get('привет мир, Memcached и nodejs для победы')).should.be.above(-1);
+    ring.find(ring.hashValue('привет мир, Memcached и nodejs для победы')).should.be.above(-1);
 
     // other odd keys
-    ring.nodes.indexOf(ring.get(1)).should.be.above(-1);
-    ring.nodes.indexOf(ring.get(0)).should.be.above(-1);
-    ring.nodes.indexOf(ring.get([])).should.be.above(-1);
-    ring.nodes.indexOf(ring.get({wtf:'lol'})).should.be.above(-1);
+    ring.find(ring.hashValue(1)).should.be.above(-1);
+    ring.find(ring.hashValue(0)).should.be.above(-1);
+    ring.find(ring.hashValue([])).should.be.above(-1);
+    ring.find(ring.hashValue({wtf:'lol'})).should.be.above(-1);
+
     ring.get({wtf:'lol'}).should.equal(ring.get({wtf:'amazing .toStringing'}));
   }
 
-, 'Replacing servers': function () {
+, 'Swapping servers': function () {
     var ring = new Hashring([
           '192.168.0.102:11212'
         , '192.168.0.103:11212'
@@ -121,9 +120,11 @@ module.exports = {
       , amazon = ring.get('justdied')
       , skynet = '192.168.0.128:11212';
 
-    ring.replace(amazon, skynet);
+    ring.swap(amazon, skynet);
     ring.cache.get("justdied").should.equal(skynet);
-    ring.cache.reset(); // clear cache
+
+    // After a cleared cache, it should still resolve to the same server
+    ring.cache.reset();
     ring.get('justdied').should.equal(skynet);
   }
 
@@ -135,16 +136,17 @@ module.exports = {
     ]);
 
     ring.remove('192.168.0.102:11212');
-    ring.nodes.indexOf('192.168.0.102:11212').should.equal(-1);
+    ring.ring.forEach(function (node) {
+      node.server.should.not.equal('192.168.0.102:11212');
+    });
   }
 
 , 'Removing last server': function () {
     var ring = new Hashring('192.168.0.102:11212');
     ring.remove('192.168.0.102:11212');
 
-    ring.nodes.should.have.length(0);
-    ring.sortedKeys.should.have.length(0);
-    Object.keys(ring.weights).should.have.length(0);
+    ring.servers.should.have.length(0);
+    ring.ring.should.have.length(0);
   }
 
   // kindly lended from `node-hash-ring` :)
