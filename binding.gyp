f620f009310517c314da7e4e8686291bbb986de0
3rd-Eden <info@3rd-Eden.com>
2013-09-13 21:24:41 +0200

    [minor] Initial stab at Node 0.11 support

--- binding.gyp
+++ binding.gyp
@@ -2,6 +2,7 @@
   "targets": [
     {
       "target_name": "hashvalue",
+      'include_dirs': ["<!(node -p -e \"require('path').dirname(require.resolve('nan'))\")"],
       "sources": [ "src/hashvalue.cc" ]
     }
   ]
--- src/hashvalue.cc
+++ src/hashvalue.cc
@@ -1,17 +1,18 @@
 #include <v8.h> 
 #include <node.h>
+#include "nan.h"
 
 using namespace v8;
 
-Handle<Value> Hasher(const Arguments& args) {
-  HandleScope scope;
-
+NAN_METHOD(Hasher) {
+  NanScope();
+  
   unsigned int hash = ((int) args[0]->NumberValue() << 24)
     | ((int) args[1]->NumberValue() << 16)
     | ((int) args[2]->NumberValue() << 8)
     | (int) args[3]->NumberValue();
 
-  return scope.Close(Number::New(hash));
+  NanReturnValue(Number::New(hash));
 }
 
 void init(Handle<Object> target) {
